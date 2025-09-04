768e3d6b40096c2203491a3e84e25621ba4ca521
Metaa4245 <cappymeta@gmail.com>
2025-09-04 15:29:38 +0200

    nil for not required error fields

--- lib/matrixir/error.ex
+++ lib/matrixir/error.ex
@@ -3,20 +3,21 @@ defmodule Matrixir.Error do
   Matrixir errors.
   """
 
+  @enforce_keys [:type, :reason]
   defstruct [
     :type,
     :reason
   ]
 
   @typedoc """
-  If the error type is of :http, then the error occurred in the Finch request.
+  If the error type is of `:http`, then the error occurred in the Finch request.
 
-  If the error type is of :decode, then the error occurred in the JSON decode.
+  If the error type is of `:decode`, then the error occurred in the JSON decode.
 
-  If the error type is of :matrix, then the error occurred in the
+  If the error type is of `:matrix`, then the error occurred in the
   Matrix API call.
 
-  If the error type is of :other, then something else happened.
+  If the error type is of `:other`, then something else happened.
   Check error_reason.
   """
   @type error_type :: :http | :decode | :matrix | :other
@@ -28,9 +29,8 @@ defmodule Matrixir.Error do
 
   The error reason in the case of a decode error is the JSON error.
 
-  The error reason in the case of a Matrix API error is
-  `Matrixir.API.Error.t/0` or (as in the case of a 404 Not Found), the HTTP
-  status code.
+  The error reason in the case of a Matrix API error is a
+  `t:Matrixir.API.Error.t/0`.
 
   The error reason in the case of other error is the error.
   """
